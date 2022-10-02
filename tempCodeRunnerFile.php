11607005d1ebdd04b9cb43966e3774d2b2b799dd
Aman Singh <81500861+singh5929@users.noreply.github.com>
2022-10-02 16:37:19 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,7 @@
+<?php
+come</Button>
+                <Button class="greenbtn" onclick="openpart('myaccount')"> My Account</Button>
+                <Button class="greenbtn" onclick="openpart('requestbook')"> Request Book</Button>
+                <Button class="greenbtn" onclick="openpart('issuereport')"> Book Report</Button>
+                <a href="index.php"><Button class="greenbtn" > LOGOUT</Button></a>
+            </div>
